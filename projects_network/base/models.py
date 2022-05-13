3eaec33acb06ae9ce41cae0b7b85c6439a6b1e2b
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2022-05-13 22:44:48 +0300

    Created projects models

--- projects_network/base/models.py
+++ projects_network/base/models.py
@@ -10,3 +10,14 @@ class Technology(models.Model):
     class Meta:
         verbose_name = 'Technology'
         verbose_name_plural = 'Technologies'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Statuses'
