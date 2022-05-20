8ca9ea6418c5e7f3a4b313de385459ec3ff5422d
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2022-05-20 20:38:13 +0300

    created community viewset

--- projects_network/communities/models.py
+++ projects_network/communities/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Community(models.Model):
     name = models.CharField(max_length=30, unique=True, verbose_name='Name')
     description = models.TextField(null=True, verbose_name='Description')
-    creating_date = models.DateField(auto_now_add=True, verbose_name='Creating date')
-    participants_number = models.PositiveIntegerField(default=0, verbose_name='Participant number')
+    creating_date = models.DateField(auto_now_add=True, blank=True, verbose_name='Creating date')
     creator = models.ForeignKey(
         to='users.User',
         on_delete=models.SET_NULL,
@@ -32,7 +31,7 @@ class Community(models.Model):
 class CommunityParticipant(models.Model):
     community = models.ForeignKey(to='communities.Community', on_delete=models.CASCADE, verbose_name='Community')
     participant = models.ForeignKey(to='users.User', on_delete=models.CASCADE, verbose_name='Participant')
-    entry_date = models.DateField(auto_now_add=True, verbose_name='Entry date')
+    entry_date = models.DateField(auto_now_add=True, blank=True, verbose_name='Entry date')
 
     def __str__(self):
         return f'{self.participant} in {self.community}'
