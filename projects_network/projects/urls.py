f47c104d0322d297dd2f36d51d2ed9df23a99875
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2022-05-25 22:22:26 +0300

    added project view

--- projects_network/projects/urls.py
+++ projects_network/projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from .views import CommunityProjectListView
+from .views import CommunityProjectListView, ProjectDetailView
 
 urlpatterns = [
-    path('communities/<int:community_id>', CommunityProjectListView.as_view(), name='community_project')
+    path('communities/<int:community_id>', CommunityProjectListView.as_view(), name='community_projects'),
+    path('communities/<int:community_id>/<int:project_id>', ProjectDetailView.as_view(), name='project')
 ]
--- projects_network/projects/views.py
+++ projects_network/projects/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -7,7 +7,14 @@ class CommunityProjectListView(ListAPIView):
     serializer_class = ProjectSerializer
 
     def get_queryset(self):
-        print(self.kwargs.get('community_id'))
         queryset = Project.objects.all()
         community_id = self.kwargs.get('community_id')
         return queryset.filter(community_id=community_id)
+
+
+class ProjectDetailView(RetrieveAPIView):
+    serializer_class = ProjectSerializer
+
+    def get_object(self):
+        project_id = self.kwargs.get('project_id')
+        return Project.objects.get(id=project_id)
